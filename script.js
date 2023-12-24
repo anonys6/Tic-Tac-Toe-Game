897ec3a8450df68c3b7d309d2ae0ec88644d14f9
anonys6 <shakibalam998@gmail.com>
2023-12-24 22:23:59 +0530

    adding new feature: check winner (done till matrix)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const itemsEl = document.getElementsByClassName("items");
 const clearBtn = document.getElementById("clear");
+const winnerPara = document.getElementById("winner-para");
 let cross = true;
+let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.style.background = "#000"
@@ -10,11 +16,15 @@ for (let i = 0; i < itemsEl.length; i++) {
     itemsEl[i].addEventListener("click", function () {
         if (itemsEl[i].innerHTML == "") {
             itemsEl[i].innerHTML = `${tickIt()}`;
+
+            updateMatrix(i);
+            console.log(board);
+
             itemsEl[i].style.cursor = "not-allowed";
         }
     })
 
-    itemsEl[i].addEventListener("mouseover", function() {
+    itemsEl[i].addEventListener("mouseover", function () {
         if (itemsEl[i].innerHTML != "") {
             itemsEl[i].style.cursor = "not-allowed";
         }
@@ -36,5 +46,27 @@ clearBtn.addEventListener("click", function () {
         itemsEl[i].style.cursor = "pointer";
         itemsEl[i].innerHTML = "";
     }
+    winnerPara.textContent = "Winner:";
+    board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
 })
 
+function updateMatrix(k) {
+    k = k + 1;
+    let count = k;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            k--;
+            if (k == 0) {
+                if (!cross) {
+                    board[i][j] = "X";
+                } else {
+                    board[i][j] = "O";
+                }
+            }
+        }
+    }
+}
